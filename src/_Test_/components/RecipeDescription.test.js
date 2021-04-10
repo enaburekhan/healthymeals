9cc64e6eb041cf05af40026322af167fce8ff165
enaburekhan <ericenaburekhan2018@gmail.com>
2021-04-10 22:30:28 +0100

    Create test for Recipe details

---
+++ src/_Test_/components/RecipeDescription.test.js
@@ -0,0 +1,42 @@
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-17-updated';
+import RecipeDetails from "../../Components/RecipeDescription";
+
+const initial = {
+  category: 'test',
+  area: 'jest',
+  ingredients: ['react', 'redux'],
+  className: 'component',    
+};
+
+const { category, area, ingredients, className } = initial;
+
+Enzyme.configure({ adapter: new Adapter() });
+const setup = () => {
+  const component = shallow(
+    <RecipeDetails
+      category={category}
+      area={area}
+      ingredients={ingredients}
+      className={className}
+    />,    
+  );
+  return component;    
+};
+
+describe('Header Component', () => {
+  let component;
+  beforeEach(() => {
+    component = setup();    
+  });
+  
+it('should render one div', () => {
+    expect(component.find('div').length).toBe(1)    
+  }); 
+  
+  it('should render upto six span elements', () => {
+    expect(component.find('span').length).toBe(6)    
+  });
+});
+
+
